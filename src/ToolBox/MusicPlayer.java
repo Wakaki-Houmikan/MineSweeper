3db2d1748ef423f53f3b0543007d7802343a0937
Wakaki_Houmikan <wakakihoumikan@hotmail.com>
2021-07-05 19:53:19 +0800

    加入注解；优化结构

--- src/ToolBox/MusicPlayer.java
+++ src/ToolBox/MusicPlayer.java
@@ -5,10 +5,31 @@ import javafx.scene.media.MediaPlayer;
 
 import java.io.File;
 
+/**
+ * <big>音乐播放器工具  Music Player</big>
+ *
+ * <p>可读取 src/Musics/BGM 里的音乐，进行音乐播放相关操作。</p>
+ *
+ * @author 分柿方橙
+ * @version ver.0.1 (2021.7.5)
+ * @since projVer.0.1 (2021.7.5)
+ */
 public class MusicPlayer {
 
-    /* 音乐序列 */
-    private final File[] musicFiles = { null,                                               // 0, 占位
+    /**
+     * File[]类型音乐列表：
+     *    0   占位未使用
+     *    1   平原 白天   巨树的记忆
+     *    2   平原 夜晚   在一颗星的旁边
+     *    3   平原 最终   漆黑
+     *    4   雪山 白天   冰霜之诗
+     *    5   雪山 夜晚   虚空中盛开的花
+     *    6   雪山 最终   乌鲁斯拉格纳
+     *    7   恶地 白天   炽热的大地
+     *    8   恶地 夜晚   心惊胆战洞穴探险
+     *    9   恶地 最终   绯红色的征兆
+     */
+    public static final File[] MUSIC_LIST = { null,                                               // 0, 占位
             new File(String.valueOf(MusicPlayer.class.getResourceAsStream(
                     "src/Musics/Memories of the Great Tree.mp3"))),                   // 1
             new File(String.valueOf(MusicPlayer.class.getResourceAsStream(
@@ -28,53 +49,83 @@ public class MusicPlayer {
             new File(String.valueOf(MusicPlayer.class.getResourceAsStream(
                     "src/Musics/Crimson Omen.mp3")))};                                // 9
 
-    /* 初始化 */
-    Media[] musicList = { null,
-        new Media(musicFiles[1].toURI().toString()),
-        new Media(musicFiles[2].toURI().toString()),
-        new Media(musicFiles[3].toURI().toString()),
-        new Media(musicFiles[4].toURI().toString()),
-        new Media(musicFiles[5].toURI().toString()),
-        new Media(musicFiles[6].toURI().toString()),
-        new Media(musicFiles[7].toURI().toString()),
-        new Media(musicFiles[8].toURI().toString()),
-        new Media(musicFiles[9].toURI().toString()),
-    };
-    MediaPlayer[] mediaPlayer = { null,
-            new MediaPlayer(musicList[1]),
-            new MediaPlayer(musicList[2]),
-            new MediaPlayer(musicList[3]),
-            new MediaPlayer(musicList[4]),
-            new MediaPlayer(musicList[5]),
-            new MediaPlayer(musicList[6]),
-            new MediaPlayer(musicList[7]),
-            new MediaPlayer(musicList[8]),
-            new MediaPlayer(musicList[9]),
+    /**
+     * MediaPlayer[]类型音乐播放列表：
+     *    0   占位未使用
+     *    1   平原 白天   巨树的记忆
+     *    2   平原 夜晚   在一颗星的旁边
+     *    3   平原 最终   漆黑
+     *    4   雪山 白天   冰霜之诗
+     *    5   雪山 夜晚   虚空中盛开的花
+     *    6   雪山 最终   乌鲁斯拉格纳
+     *    7   恶地 白天   炽热的大地
+     *    8   恶地 夜晚   心惊胆战洞穴探险
+     *    9   恶地 最终   绯红色的征兆
+     */
+    public static final MediaPlayer[] MEDIA_PLAYER = { null,
+            new MediaPlayer(new Media(MUSIC_LIST[1].toURI().toString())),
+            new MediaPlayer(new Media(MUSIC_LIST[2].toURI().toString())),
+            new MediaPlayer(new Media(MUSIC_LIST[3].toURI().toString())),
+            new MediaPlayer(new Media(MUSIC_LIST[4].toURI().toString())),
+            new MediaPlayer(new Media(MUSIC_LIST[5].toURI().toString())),
+            new MediaPlayer(new Media(MUSIC_LIST[6].toURI().toString())),
+            new MediaPlayer(new Media(MUSIC_LIST[7].toURI().toString())),
+            new MediaPlayer(new Media(MUSIC_LIST[8].toURI().toString())),
+            new MediaPlayer(new Media(MUSIC_LIST[9].toURI().toString())),
     };
 
-    /* 序列号：最近一次播放的曲子 */
-    byte latest;
+    /**
+     *  最近一次播放曲子的序号：
+     *    0   占位未使用
+     *    1   平原 白天   巨树的记忆
+     *    2   平原 夜晚   在一颗星的旁边
+     *    3   平原 最终   漆黑
+     *    4   雪山 白天   冰霜之诗
+     *    5   雪山 夜晚   虚空中盛开的花
+     *    6   雪山 最终   乌鲁斯拉格纳
+     *    7   恶地 白天   炽热的大地
+     *    8   恶地 夜晚   心惊胆战洞穴探险
+     *    9   恶地 最终   绯红色的征兆
+     */
+    public byte recentMusicIndex;
 
-    /* 音乐关闭 */
+    /**
+     * <big>暂停音乐  Pause Music</big>
+     *
+     * <p>暂停正在播放的所有BGM。</p>
+     */
     public void musicOff(){
         for (byte i = 1; i <= 9; i++){
-            mediaPlayer[i].pause();
+            MEDIA_PLAYER[i].pause();
         }
     }
 
-    /* 音乐切换 */
-    public void musicToggle(byte order, double volume){
+    /**
+     * <big>切换音乐  Cut Music</big>
+     *
+     * <p>以当前音量无限循环播放指定BGM，并暂停其他所有BGM。</p>
+     *
+     * @param order 设置音乐序号
+     * @param volume 设置音量
+     */
+    public void musicCut(byte order, double volume){
         musicOff();
-        latest = order;
-        mediaPlayer[order].play();
-        mediaPlayer[order].setCycleCount(999);
-        mediaPlayer[order].setVolume(volume);
+        recentMusicIndex = order;
+        MEDIA_PLAYER[order].play();
+        MEDIA_PLAYER[order].setCycleCount(999);
+        MEDIA_PLAYER[order].setVolume(volume);
     }
 
-    /* 音量调节 */
+    /**
+     * <big>调节音量  Adjust Volume</big>
+     *
+     * <p>将音量设置为给定百分值。</p>
+     *
+     * @param volume 设置音量
+     */
     public void musicVolume(double volume){
         for (byte i = 1; i <= 9; i++){
-            mediaPlayer[i].setVolume(volume);
+            MEDIA_PLAYER[i].setVolume(volume);
         }
     }
 }
