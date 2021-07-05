41a1e0e613e5a0e2d45ccd04d92d74439b1e541f
Wakaki_Houmikan <wakakihoumikan@hotmail.com>
2021-07-05 15:53:06 +0800

    加入游戏图标读取工具

---
+++ src/Graphics/ICSetter.java
@@ -0,0 +1,44 @@
+package Graphics;
+
+import javafx.scene.image.ImageView;
+
+/* Icon Setter */
+public class ICSetter {
+    /* 基底 */
+    public final ImageView bluBase = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockBaseBlu.png")));
+    public final ImageView greBase = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockBaseGre.png")));
+    public final ImageView redBase = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockBaseRed.png")));
+
+    /* 黑暗：用于夜晚 */
+    public final ImageView bluDark = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockBluDark.png")));
+    public final ImageView greDark = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockGreDark.png")));
+    public final ImageView redDark = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockRedDark.png")));
+    /* 普通：用于白天、夜晚鼠标划过 */
+    public final ImageView bluNorm = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockBluNorm.png")));
+    public final ImageView greNorm = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockGreNorm.png")));
+    public final ImageView redNorm = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockRedNorm.png")));
+    /* 明亮：用于白天鼠标划过 */
+    public final ImageView bluShiny = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockBluShiny.png")));
+    public final ImageView greShiny = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockGreShiny.png")));
+    public final ImageView redShiny = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockRedShiny.png")));
+
+    /* 黑暗旗子：夜晚插旗 */
+    public final ImageView bluDarkFlag = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockBluDarkFlag.png")));
+    public final ImageView greDarkFlag = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockGreDarkFlag.png")));
+    public final ImageView redDarkFlag = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockRedDarkFlag.png")));
+    /* 普通旗子：白天插旗 */
+    public final ImageView bluNormFlag = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockBluNormFlag.png")));
+    public final ImageView greNormFlag = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockGreNormFlag.png")));
+    public final ImageView redNormFlag = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/blockRedNormFlag.png")));
+
+    /* 数字1~8 */
+    public final ImageView num1 = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/num1.png")));
+    public final ImageView num2 = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/num2.png")));
+    public final ImageView num3 = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/num3.png")));
+    public final ImageView num4 = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/num4.png")));
+    public final ImageView num5 = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/num5.png")));
+    public final ImageView num6 = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/num6.png")));
+    public final ImageView num7 = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/num7.png")));
+    public final ImageView num8 = new ImageView(String.valueOf(ICSetter.class.getResource("/Graphics/IC/num8.png")));
+
+}
